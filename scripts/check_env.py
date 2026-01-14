5be8fda69cbcc045ebdcf236eb84f56f675700c7
Aljerme <aljermez@oregonstate.edu>
2026-01-14 08:27:55 -0800

    Add venv requirements and env check script

---
+++ scripts/check_env.py
@@ -0,0 +1,10 @@
+import platform
+import sys
+
+import numpy as np
+import pandas as pd
+
+print("Python:", sys.version)
+print("Platform:", platform.platform())
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
